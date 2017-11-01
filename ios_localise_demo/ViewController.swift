741741c8600a72319037633b15e02d7b09d1ff15
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-11-01 13:32:35 +0900

    add localizing code

--- ios_localise_demo/ViewController.swift
+++ ios_localise_demo/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Yuu Ogasa on 2017/11/01.
 //  Copyright © 2017年 SundayCarpenter. All rights reserved.
-//
+//　https://qiita.com/Sim_progra/items/c9b04377dfa1425085c3
 
 import UIKit
 
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myName = "sim"
+        
+        print(NSLocalizedString("welcome", comment: ""))
+        print(String(format: NSLocalizedString("name", comment: ""), myName))
+        print(String(format: NSLocalizedString("numbers", comment: ""), 10, 20))
+        print(String(format: NSLocalizedString("words", comment: ""), "Apple", "Mac"))
+        
+        /*
+         # 日本語環境
+         ハローワールド！
+         あなたの名前は sim です。
+         壱: 10, 弐: 20
+         弐: Mac, 壱: Apple
+         
+         #それ以外
+         Hello, World!
+         Your name is sim.
+         one: 10, two: 20
+         one: Apple, two: Mac
+         */
     }
 
     override func didReceiveMemoryWarning() {
