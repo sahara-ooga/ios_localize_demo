9dd3752994537d63410172da7f3d73df745b70e0
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-11-01 13:18:00 +0900

    Initial Commit

---
+++ ios_localise_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_localise_demo
+//
+//  Created by Yuu Ogasa on 2017/11/01.
+//  Copyright © 2017年 SundayCarpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
